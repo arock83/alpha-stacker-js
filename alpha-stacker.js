ba3a99771c1d99f3bc0630a5fd4efaff98948509
Andrew Rock <Andrew Rock>
2017-05-01 20:31:08 -0500

    there are your spaces

--- alpha-stacker.js
+++ alpha-stacker.js
@@ -4,7 +4,14 @@ var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p",
 // The stackLetter function should accept the array as the sole argument
 function stackLetters (theAlphabetArray) {
     var stacker = "";
+    var spaceCounter = 0;
      for (n=0; n < theAlphabetArray.length; n+=1) {
+     	if (spaceCounter <3) {
+     		spaceCounter +=1;
+     	} else {
+     		stacker += " ";
+     		spaceCounter = 1;
+     	}
      	stacker += alphabet[n];
      	console.log(stacker);
 
